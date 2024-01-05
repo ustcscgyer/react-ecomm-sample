7f5ff06972a157e072a9efb180ebeb80e5655ddd
Giovanni Ge <ustcscgyer@gmail.com>
2024-01-04 16:50:28 -0800

    Updates

---
+++ precache-manifest.e87f481975c06e796dd35d9c446b857c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cf9fa4b4b62665addcaaed29ff6301e",
+    "url": "/react-ecomm-sample/index.html"
+  },
+  {
+    "revision": "16d53f9bee15377e504e",
+    "url": "/react-ecomm-sample/static/css/main.c35e003f.chunk.css"
+  },
+  {
+    "revision": "6ea7e4cd466d55cfb758",
+    "url": "/react-ecomm-sample/static/js/2.ffbf521d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-ecomm-sample/static/js/2.ffbf521d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16d53f9bee15377e504e",
+    "url": "/react-ecomm-sample/static/js/main.83a720bd.chunk.js"
+  },
+  {
+    "revision": "997a53edebca417197e6",
+    "url": "/react-ecomm-sample/static/js/runtime-main.e253932b.js"
+  },
+  {
+    "revision": "e4072dc0ea58c0cfbe4f4a0ad941b501",
+    "url": "/react-ecomm-sample/static/media/banner.e4072dc0.jpg"
+  }
+]);
